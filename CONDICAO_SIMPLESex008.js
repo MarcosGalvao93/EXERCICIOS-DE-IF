6aa35d66f0c1f526d69bdb29e12c4e423a6a6692
Marcos_Poke <158784230+MarcosGalvao93@users.noreply.github.com>
2024-03-06 01:35:18 -0300

    Add files via upload

---
+++ CONDICAO_SIMPLESex008.js
@@ -0,0 +1,10 @@
+var speed = 5000000
+console.log(`A velocidade do soco é de ${speed}km/h`)
+if 
+(speed > 300000)
+{
+    console.log(`INCRIVEL!! SEU SOCO ULTRAPASSOU A VELOCIDADE DA LUZ!!!`)
+
+    
+}
+console.log(`METEORO DE PEGASO!!`)
